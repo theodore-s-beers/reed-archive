f1a70f6e12a1a39f1427bd44fe9c9470bf8a3bf9
Theo Beers <theo.beers@fu-berlin.de>
2021-07-08 11:38:39 +0200

    Fix dependency issue

--- build.sh
+++ build.sh
@@ -1,48 +1,35 @@
-#! /opt/homebrew/bin/bash
+#! /usr/bin/env bash
+
+set -Eeuo pipefail
 
 fd index.md \
---exec pandoc {} \
--f markdown-auto_identifiers \
--H '{//}'/desc.html \
--H components/style-links.html \
--B components/header.html \
--A components/footer.html \
--A components/script-link.html \
--o '{.}'.html &&
-
-pandoc index-0.md \
--f markdown-auto_identifiers \
--H desc.html \
--H components/style-links-main.html \
--B components/header-main.html \
--A components/script-link-main.html \
--o index.html &&
-
-echo "Pandoc finished" &&
-
-npx sort-package-json &&
-
-npm install &&
-
-npm run minify &&
-
-fd index.html --exec sd '<style>.*</style>' '' {}
-
-echo "sd round 1 finished" &&
-
-fd index.html --exec sd '<p class="auth.*</p></header>' '</header>' {} &&
-
-echo "sd round 2 finished" &&
-
-fd index.html --exec sd '<hr><ol>' '<ol>' {} &&
-
-echo "sd round 3 finished" &&
-
-fd index.html \
---exec sd 'a href="http' 'a target="_blank" rel="noopener" href="http' {} &&
-
-echo "sd round 4 finished" &&
-
-npm run prettify &&
-
-npm run standardize
+	--exec pandoc {} \
+	-f markdown-auto_identifiers \
+	-H '{//}'/desc.html \
+	-H components/style-links.html \
+	-B components/header.html \
+	-A components/footer.html \
+	-A components/script-link.html \
+	-o '{.}'.html &&
+	pandoc index-0.md \
+		-f markdown-auto_identifiers \
+		-H desc.html \
+		-H components/style-links-main.html \
+		-B components/header-main.html \
+		-A components/script-link-main.html \
+		-o index.html &&
+	echo "Pandoc finished" &&
+	sort-package-json &&
+	npm install &&
+	npm run minify &&
+	fd index.html --exec sd '<style>.*</style>' '' {} &&
+	echo "sd round 1 finished" &&
+	fd index.html --exec sd '<p class="auth.*</p></header>' '</header>' {} &&
+	echo "sd round 2 finished" &&
+	fd index.html --exec sd '<hr><ol>' '<ol>' {} &&
+	echo "sd round 3 finished" &&
+	fd index.html \
+		--exec sd 'a href="http' 'a target="_blank" rel="noopener" href="http' {} &&
+	echo "sd round 4 finished" &&
+	npm run prettify &&
+	npm run standardize
